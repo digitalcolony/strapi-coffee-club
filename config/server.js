41e15d3874a65106071a827e5b4fb6d99ea22d81
Michael Allen Smith <digitalcolony@gmail.com>
2021-08-19 17:59:04 -0700

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a51f7b61685e81f25acf453d05d15778'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3ff77842-731f-4a71-b104-55d3d77f02a4'
+};
